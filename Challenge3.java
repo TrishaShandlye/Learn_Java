0c2d55e98b4dfbb05c1d494d7c21959d8618bb7d
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-17 15:28:45 +0530

    added files

---
+++ Challenge3.java
@@ -0,0 +1,7 @@
+// show the patterns "Right Half Pyramid","Reverse Right Half Pyramid" and "Left Half Pyramid" using single print statement:
+import java.util.*;
+public class Challenge3{
+    public static void main(String[] args){
+        
+    }
+}
--- Variables.java
+++ Variables.java
@@ -4,13 +4,20 @@ public class Variables{
         int myNum;
         myNum= 04;
         System.out.println(myNum);
+
          myNum= 244;
         System.out.println(myNum);
+
         boolean isVegetarian = true;
         System.out.println(isVegetarian);
+
         float myFloat= 6.1f;
         System.out.println(myFloat);
+
         String wishes ="Good Morning Everybody :)";
         System.out.println(wishes);
+
+        char myCharacter='A';
+        System.out.println( myCharacter);
     }
 }
