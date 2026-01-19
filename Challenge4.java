dcf3a7e8acce5c3368008ee3f508e83315d37d81
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-20 00:34:04 +0530

    Added Files

---
+++ Challenge4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Challenge4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter Two  Numbers:");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c=a+b;
+        System.out.println("Addition is:" +c);
+
+    }
+    
+}
--- userInput.java
+++ userInput.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class userInput{
     public static void main(String[]args){
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter your Name:");
-        String name = input.nextLine();
-        System.out.println("Welcome" +name);
-        System.out.println(name+ "Also tell me your age");
-        int age=  input.nextInt();
+        String name = sc.nextLine();
+        System.out.println("Welcome " +name);
+        System.out.println(name+ " Also tell me your age");
+        int age=  sc.nextInt();
         System.out.println("Your Age Is: "+age );
     }
 }
