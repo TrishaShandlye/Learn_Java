4d42d1fffed40ad58444acc3496181e279c9cb52
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-21 22:51:52 +0530

    loop practice

---
+++ Loop.java
@@ -0,0 +1,10 @@
+public class Loop {
+    public static void main(String[] args) {
+        int num = 1; //initialization
+        while(num<=10){ //condition
+            System.out.println(num);
+            num=num+1; //updation
+        }
+    }
+    
+}
