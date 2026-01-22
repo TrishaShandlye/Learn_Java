51c96ac90eac4589422937b8f462089082a632e4
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-22 23:48:53 +0530

    Updated Files

---
+++ MultiplicationTable.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Table Station...");
+        System.out.println("Please Enter Your Number:");
+        int num = sc.nextInt();
+        printMultiplicationTable(num);
+    }
+
+    public static void printMultiplicationTable(int num) {
+
+    }
+}
