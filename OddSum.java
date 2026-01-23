46f04aa06a7e82c20bad3b4f36034caee0abfcf9
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-23 22:35:21 +0530

    Added Files

--- OddSum.java
+++ OddSum.java
@@ -3,9 +3,28 @@ import java.util.Scanner;
 public class OddSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Welcome to Odd Addition...");
-        System.out.print("Please Enter Your Number:");
+        System.out.print("Please Enter Your Number: ");
         int num = sc.nextInt();
+
+        int sum = oddSum(num);
+
+        System.out.println("Odd Sum till " + num + " is " + sum);
+
+        sc.close();
     }
-    public static int oddSum()
+
+    public static int oddSum(int n) {
+        int sum = 0;
+        int i = 1;
+
+        while (i <= n) {
+            sum = sum + i;
+            i = i + 2;
+        }
+
+        return sum;
+    }
 }
+
