63575bd05fb2db741f984903fb25925a13d53455
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-23 22:00:58 +0530

    Added Files

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -4,12 +4,17 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to Table Station...");
-        System.out.println("Please Enter Your Number:");
+        System.out.print("Please Enter Your Number:");
         int num = sc.nextInt();
         printMultiplicationTable(num);
     }
 
     public static void printMultiplicationTable(int num) {
+        int i=1;
+        while(i<=10){
+            System.out.println(num+"x"+i+"="+(num*i));
+            i++;
+        }
 
     }
 }
---
+++ OddSum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class OddSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Odd Addition...");
+        System.out.print("Please Enter Your Number:");
+        int num = sc.nextInt();
+    }
+    public static int oddSum()
+}
