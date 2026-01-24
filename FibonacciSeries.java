92ee3fa19637c57d3e8177b438d38acee4afbc22
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-25 00:06:11 +0530

    Practice Problem

---
+++ FibonacciSeries.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Fibonacci Series Station...");
+        System.out.print("Please Enter Your Number:");
+        int num = sc.nextInt();
+        System.out.println("Fibonacci Series will be:");
+        fibonacci(num);
+    }
+    public static void fibonacci(int num){
+        if (num<0) return ;
+        System.out.println("0");
+        if(num==0) return ;
+        System.out.println("1");
+        int first =0; 
+        int second= 1;
+        while(first+second<=num){
+            int third;
+            third=first+second;
+            System.out.println(third+" ");
+            first=second;
+            second=third;
+        }
+
+        
+    }
+}
