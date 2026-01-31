45bda459130df35773cdab1b75537960c748552b
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-31 23:08:42 +0530

    Practice Problem

--- _52ArraySumAverage.java
+++ _52ArraySumAverage.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ArraySumAverage {
+public class _52ArraySumAverage {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
