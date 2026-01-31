ce702d722a44f26c54a3f90fa32b56a8d73abe30
TrishaShandlye <shandlyetrisha@gmail.com>
2026-01-31 23:12:26 +0530

    Added Files

--- _53OccurrenceOfElements.java
+++ _53OccurrenceOfElements.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class OccurrenceOfElements {
+public class _53OccurrenceOfElements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
