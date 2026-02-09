7f7aef66b294d2c43b591a1816ffa3fada9053ad
TrishaShandlye <shandlyetrisha@gmail.com>
2026-02-09 21:33:58 +0530

    Added Files

---
+++ _56IsSortedArray.java
@@ -0,0 +1,37 @@
+ public class _56IsSortedArray {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Array Sorting Checkpost///");
+        int[] numArray=_53ArrayUtility.inputArray();
+        boolean inc=IsIncreasing(numArray);
+        boolean dec=IsDecreasing(numArray);
+        if(inc || dec){
+            System.out.println("Your Array Is Sorted..");
+        }
+        else{
+            System.out.println("Your Array Is Not Sorted..");
+        }
+    }
+    public static boolean IsIncreasing(int[]numArray)
+    {
+        int i=1;
+        while(i<numArray.length){
+            if(numArray[i]<numArray[i-1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+    public static boolean IsDecreasing(int[]numArray){
+        int i=1;
+        while(i<numArray.length){
+            if(numArray[i]>numArray[i-1]){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+} {
+    
+}
