04e35ca62318c4fe7a0c6fa2cddad4d44dabbc7d
TrishaShandlye <shandlyetrisha@gmail.com>
2026-02-13 22:33:25 +0530

    Updated Files

--- _56IsSortedArray.java
+++ _56IsSortedArray.java
@@ -1,4 +1,4 @@
- public class _56IsSortedArray {
+  public class _56IsSortedArray {
     public static void main(String[] args) {
         System.out.println("Welcome to Array Sorting Checkpost///");
         int[] numArray=_53ArrayUtility.inputArray();
@@ -32,4 +32,4 @@
         }
         return true;
     }
-} 
+}  
