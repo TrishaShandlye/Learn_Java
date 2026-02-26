5aaf4c3df22b9ecfbd26833913d7effec717ea6b
TrishaShandlye <shandlyetrisha@gmail.com>
2026-02-26 11:24:21 +0530

    Added new Files

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    void hello() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Hello h = new Hello();  
+        h.hello();              
+} {
+    
+}
